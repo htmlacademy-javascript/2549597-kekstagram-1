e128db51f582ee0b0e76af24452552b3547f1cf4
Ilia <ilya.belyaev.95@list.ru>
2024-07-03 15:02:16 +0300

    Исправление стилистических ошибок.

--- js/functions.js
+++ js/functions.js
@@ -1,82 +1,65 @@
-function isPalindrome(text) {
-  const modifiedText = text.toLowerCase().replaceAll(' ', '');
-  const halfLengthModifiedText = Math.ceil(modifiedText.length / 2);
+const isPalindrome = (text) => {
+  const letters = text.toLowerCase().replaceAll(' ', '');
+  const lettersCenter = Math.ceil(letters.length / 2);
 
-  for (let i = 0; i < halfLengthModifiedText; i++) {
-    if (modifiedText[i] !== modifiedText[modifiedText.length - 1 - i]) {
+  for (let i = 0; i < lettersCenter; i++) {
+    if (letters[i] !== letters[letters.length - 1 - i]) {
       return false;
     }
   }
 
   return true;
-}
+};
 
 isPalindrome('топот');
-
 isPalindrome('ДовОд');
-
 isPalindrome('Кекс');
-
 isPalindrome('Лёша на полке клопа нашёл ');
 
-function getNumberFromString(text) {
+const getNumberFromString = (text) => {
   const modifiedText = text.toString().replaceAll(' ', '');
-  let resultNumber = '';
+  let result = '';
 
   for (const letter of modifiedText){
     if (!Number.isNaN(Number(letter))) {
-      resultNumber = resultNumber + letter;
+      result = result + letter;
     }
   }
 
-  return parseInt(resultNumber,10);
-}
+  return parseInt(result, 10);
+};
 
 getNumberFromString('2023 год');
-
 getNumberFromString('ECMAScript 2022');
-
 getNumberFromString('1 кефир, 0.5 батона');
-
 getNumberFromString('агент 007');
-
 getNumberFromString('а я томат');
-
 getNumberFromString(2023);
-
 getNumberFromString(-1);
-
 getNumberFromString(1.5);
 
-function getReplacingString(currentText, textLength, extraText) {
+const getReplacingString = (currentText, textLength, extraText) => {
   if (textLength <= currentText.length) {
     return currentText;
   }
 
   let freeLength = textLength - currentText.length;
   const countOfRepeat = Math.floor(freeLength / extraText.length);
-  let newCurrentText = extraText.repeat(countOfRepeat) + currentText;
+  const newCurrentText = extraText.repeat(countOfRepeat) + currentText;
 
   freeLength = textLength - newCurrentText.length;
-  newCurrentText = extraText.slice(0, freeLength) + newCurrentText;
 
-  return newCurrentText;
-}
+  return extraText.slice(0, freeLength) + newCurrentText;
+};
 
 getReplacingString('1', 2, '0');
-
 getReplacingString('1', 4, '0');
-
 getReplacingString('q', 4, 'werty');
-
 getReplacingString('q', 4, 'we');
-
 getReplacingString('qwerty', 4, '0');
 
 const isTextLengthValid = (currentText, maxLength) => currentText.length <= maxLength;
 
 isTextLengthValid('проверяемая строка', 20);
-
 isTextLengthValid('проверяемая строка', 18);
-
 isTextLengthValid('проверяемая строка', 10);
