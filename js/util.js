f4d3cd8627b8e5f00ca03cb8dabc9b54c96e3281
Ilia <ilya.belyaev.95@list.ru>
2024-07-11 16:38:17 +0300

    Разделил на модули main.js.

---
+++ js/util.js
@@ -0,0 +1,11 @@
+const getRandomValue = (min, max) => {
+  const lower = Math.ceil(Math.min(min, max));
+  const upper = Math.floor(Math.max(min, max));
+  const result = Math.random() * (upper - lower + 1) + lower;
+
+  return Math.floor(result);
+};
+
+const getRandomArrayElement = (elements) => elements[getRandomValue(0, elements.length - 1)];
+
+export {getRandomValue, getRandomArrayElement};
