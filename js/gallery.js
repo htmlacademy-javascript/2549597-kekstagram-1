bee26f5f3da1da17a2f2f684655a2147a350dfb1
Ilia <ilya.belyaev.95@list.ru>
2024-08-29 14:10:45 +0300

    Домашняя работа 12. Работа с сортировкой фото.

--- js/gallery.js
+++ js/gallery.js
@@ -1,7 +1,12 @@
 import {showBigPhoto} from './big-picture.js';
+import {filterId} from './sorting-photos.js';
+import {getRandomArrayElement} from './utils.js';
+
+const QUANTITY_UNIQUE_PHOTO = 10;
 
 const imageTemplate = document.querySelector('#picture').content.querySelector('.picture');
 const pictures = document.querySelector('.pictures');
+const imageFilters = document.querySelector('.img-filters');
 
 let photos = [];
 
@@ -16,19 +21,46 @@ const createPhotoElement = (data) => {
   return image;
 };
 
+const getRandomPhotos = (data) => {
+  const mass = new Set();
+  while (mass.size < QUANTITY_UNIQUE_PHOTO) {
+    mass.add(getRandomArrayElement(data));
+  }
+
+  return Array.from(mass).toSorted((firstElem, secondElem) => firstElem.id - secondElem.id);
+};
+
+
+const getDiscussedPhotos = (data) => data.toSorted((firstElEM, secondElem) => secondElem.comments.length - firstElEM.comments.length);
+
+const getNewPhotoElements = (data) => {
+  if (filterId === 'filter-random') {
+    return getRandomPhotos(data);
+  }else {
+    return getDiscussedPhotos(data);
+  }
+};
+
 export const renderGallery = (data) => {
   const fragment = document.createDocumentFragment();
+
+  if (filterId !== 'filter-default') {
+    data = getNewPhotoElements(data);
+  }
 
   data.forEach((element) => {
     fragment.append(createPhotoElement(element));
   });
 
+  pictures.innerHTML = '';
   pictures.append(fragment);
 };
 
 export const initGallery = (photoData) => {
   renderGallery(photoData);
   photos = photoData;
+
+  imageFilters.classList.remove('img-filters--inactive');
 };
 
 pictures.addEventListener('click', (evt) => {
--- js/main.js
+++ js/main.js
@@ -1,14 +1,19 @@
 import './gallery.js';
 import './big-picture.js';
 import './form.js';
+import './sorting-photos.js';
 
 import {getData} from './api.js';
 import {initGallery} from './gallery.js';
 import {showAlert} from './dialogs.js';
+import {setPhotoFilter} from './sorting-photos.js';
+import {debounce} from './utils.js';
 
 getData()
   .then((photoData) => {
     initGallery(photoData);
+    setPhotoFilter(debounce(
+      () => initGallery(photoData)));
   }).catch(
     () => {
       showAlert();
